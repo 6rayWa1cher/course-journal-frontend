c91a49024c307b28e13605af2dd687e30a73c180
Konstantin Grigorev <info@a6raywa1cher.com>
2022-05-25 16:17:59 +0300

    Add BackButton to employee explorer and settings page

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,4 +1,7 @@
+import { Grid, Paper } from '@mui/material';
 import { Box } from '@mui/system';
+import BackButton from 'components/buttons/BackButton';
+import Title from 'components/Title';
 import { useDocumentTitle } from 'utils/hooks';
 import AuthUserModule from './AuthUserModule';
 
@@ -6,9 +9,21 @@ const SettingsPage = () => {
   useDocumentTitle('Настройки');
 
   return (
-    <Box>
-      <AuthUserModule />
-    </Box>
+    <>
+      <Paper sx={{ p: 2 }}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <BackButton />
+          </Grid>
+          <Grid item xs>
+            <Title>Преподаватели</Title>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Box sx={{ pt: 2 }}>
+        <AuthUserModule />
+      </Box>
+    </>
   );
 };
 
